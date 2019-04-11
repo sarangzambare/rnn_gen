9014cfcc92ba1245a647834da65e38cbba125cc4
Sarang Zambare <thesarng@gmail.com>
2019-04-10 20:57:53 -0700

    added data preparation method

--- main.py
+++ main.py
@@ -2,9 +2,28 @@ import tensorflow as tf
 import pandas as pd
 from tensorflow.keras.layers import Activation, GRU, Input, Dropout, BatchNormalization, Dense, concatenate
 import tensorflow.keras as keras
+import numpy as np
 
 
-#data = pd.read_csv('data/bucket_HY_before_standarize.csv')
+data = pd.read_csv('data/bucket_HY_before_standarize.csv')
+
+
+def prepare_data(data):
+    payments = data.Payment
+    nulls = payments.isnull()
+    out = np.zeros((len(payments),2))
+
+    for i in range(len(payments)):
+        valid = [payments[i],1]
+        non_valid = [0,0]
+
+        if(nulls[i]):
+            out[i] = non_valid
+        else:
+            out[i] = valid
+
+
+    return out
 
 class CONFIG:
     INPUT_SHAPE = (3,2)
