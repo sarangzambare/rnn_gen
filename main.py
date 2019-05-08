2215509e390a27784a28c2bd8d6986ca6a3dc01c
Sarang Zambare <thesarng@gmail.com>
2019-05-08 14:32:11 -0700

    updated model to predict 6 features.

--- main.py
+++ main.py
@@ -8,22 +8,34 @@ import time
 import matplotlib.pyplot as plt
 
 
-data = pd.read_csv('data/bucket_HY_before_standarize.csv')
+data = pd.read_csv('data/HY_cleaned.csv')
+
+
+'''
+(Currently perdicting:
+1. Payment
+2. Number_of_observations
+3. Payment_std
+4. Payment_min
+5. Payment_max
+6. Null probability
+'''
 
 class CONFIG:
     LOOK_WINDOW = 3
-    INPUT_SHAPE = (LOOK_WINDOW,2)
+    INPUT_SHAPE = (LOOK_WINDOW,6)
     DATA_DIR = 'data/'
 
 
 def prepare_data(data):
     payments = data.Payment
+    d = data[['Payment','Number_of_observations','Payment_std','Payment_min','Payment_max']]
     nulls = payments.isnull()
-    out = np.zeros((len(payments),2))
+    out = np.zeros((len(payments),6))
 
     for i in range(len(payments)):
-        valid = [payments[i]/1e5,1]
-        non_valid = [0,0]
+        valid = [payments[i]/1e9,d.Number_of_observations[i],d.Payment_std[i]/1e9,d.Payment_min[i]/1e9,d.Payment_max[i]/1e9,1]
+        non_valid = [0,0,0,0,0,0]
 
         if(nulls[i]):
             out[i] = non_valid
@@ -34,13 +46,11 @@ def prepare_data(data):
     return out
 
 prep_data = prepare_data(data)
-
 prep_data.shape
 prep_data = prep_data[:-1]  #getting the len to be divisible by 4 = (LOOK_WINDOW + 1)
-
 num_samples = int(len(prep_data)/(CONFIG.LOOK_WINDOW+1))
-x_train = np.zeros((num_samples,3,2))
-y_train = np.zeros((num_samples,2))
+x_train = np.zeros((num_samples,3,6))
+y_train = np.zeros((num_samples,6))
 
 for i in range(num_samples):
     x_out = []
@@ -70,8 +80,8 @@ def custom_loss(model,x,y):
     #assert(model.output.shape == OUTPUT_SHAPE)
 
     y_pred = model(x)
-    nan_loss = CrossEntropy(y_pred[:,1],y[:,1])
-    payments_loss = tf.reduce_mean(tf.math.square(y[:,0] - y_pred[:,0]))
+    nan_loss = CrossEntropy(y_pred[:,5],y[:,5])
+    payments_loss = tf.reduce_mean(tf.math.square(y[:,:-1] - y_pred[:,:-1]))
     final_loss = nan_loss + payments_loss
     print("nan_loss is :" + str(nan_loss) + 'payments_loss is: ' + str(payments_loss))
     return final_loss
@@ -98,7 +108,8 @@ def base_model(input_shape):
     x = Dropout(0.5)(x)
     x = BatchNormalization()(x)
     x = Dense(8,activation='relu')(x)
-    y_payment = Dense(1,activation=None)(x)
+    x = Dense(6,activation='relu')(x)
+    y_payment = Dense(5,activation='relu')(x)
     y_prob = Dense(1,activation='sigmoid')(x)
 
     x = concatenate([y_payment,y_prob])
@@ -109,27 +120,29 @@ def base_model(input_shape):
 
 
 x_train[:10]
+
 model = base_model(CONFIG.INPUT_SHAPE)
 
 #model.summary()
 
 epochs = 2000
-learning_rate = 1e-3
+learning_rate = 1e-4
 optimizer = tf.keras.optimizers.Adam(learning_rate)
 
-for i in tqdm(range(1, epochs+1)):
-    gradients, loss = compute_gradients(model,x_train[:500],y_train[:500])
+for i in range(1, epochs+1):
+    gradients, loss = compute_gradients(model,x_train[:400],y_train[:400])
     apply_gradients(optimizer,gradients,model.trainable_variables)
 
 
 
 model.save('1000epochs.h5')
-
+model = tf.keras.models.load_model('1000epochs.h5')
 #model.save('big_model_2000epochs.h5')
 
 x_test = x_train[200:]
 y_preds_test = model(x_test)
 y_preds_test = np.asarray(y_preds_test)
+y_preds_test[:30]
 y_test = y_train[200:]
 
 for i in range(len(y_preds_test)):
